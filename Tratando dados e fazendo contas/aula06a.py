ffea7810b387b70719a4e86db0e0152dc0a17bfe
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-01-07 17:48:13 -0300

    new exercisies

---
+++ Tratando dados e fazendo contas/aula06a.py
@@ -0,0 +1,6 @@
+numero1=int(input("digite um numero: "))
+numero2=int(input("digite outro numero: "))
+resultado=numero1+numero2
+print("")
+#print("A soma entre",numero1,"e",numero2,"é igual a", resultado)
+print("A soma entre {} e {} é igual a {}".format(numero1,numero2,resultado))
---
+++ Tratando dados e fazendo contas/desafio003.py
@@ -0,0 +1,5 @@
+n1=int(input("Digite um numero: "))
+n2=int(input("Digite outro numero: "))
+soma=n1+n2
+print("")
+print("A soma entre {} e {} é igual a {}".format(n1,n2,soma))
