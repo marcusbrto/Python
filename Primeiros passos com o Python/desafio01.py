605381a58eca7d75b9d6c0b3038b0f9eef2f4676
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-01-07 16:19:39 -0300

    atualização

--- Primeiros passos com o Python/desafio01.py
+++ Primeiros passos com o Python/desafio01.py
@@ -1,6 +1,2 @@
-Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> nome=input("Qual seu nome?")
-Qual seu nome?Cleuza
->>> print("Olá",nome,", seja muito bem vindo(a)!")
-Olá Cleuza , seja muito bem vindo(a)!
+nome=input("Qual seu nome?")
+print("Olá",nome,", seja muito bem vindo(a)!")
