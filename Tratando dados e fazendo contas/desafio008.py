fd75485cdb9221decf72cb94f1bc7e6701295418
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-01-07 22:41:51 -0300

    exercicios

---
+++ Tratando dados e fazendo contas/desafio008.py
@@ -0,0 +1,4 @@
+metros = int(input("Digite um valor em metros: "))
+centimetros = metros*100
+milimetros = metros*1000
+print("Sua metragem {} é igual a {} centimetros e {} milimetros".format(metros,centimetros,milimetros))
---
+++ Tratando dados e fazendo contas/desafio010.py
@@ -0,0 +1,3 @@
+real = float(input("Digite um valor real: "))
+dolares = real / 3.27
+print("Com o valor de {} você consegue comprar {:.2f} dólares!".format(real,dolares))
