cb97fc4d498557bbd1fb8d0015f2e99b8244eec3
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-01-08 10:16:02 -0300

    correção

--- Tratando dados e fazendo contas/desafio006.py
+++ Tratando dados e fazendo contas/desafio006.py
@@ -1,2 +1,2 @@
 digito=int(input("Digite um número: "))
-print("O dobro de {} é {}, o triplo é {} e a raiz quadrada é {}!".format(digito,digito*2,digito*3,digito**2))
+print("O dobro de {} é {}, o triplo é {} e a raiz quadrada é {}!".format(digito,digito*2,digito*3,digito**(1/2)))
