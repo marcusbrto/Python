9f16e97eafd66f76e02c7d5379d880f37705dd6e
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-01-15 10:44:49 -0300

    Update desafio049.py

--- Mundo 02/Repetições em Python (for)/desafio049.py
+++ Mundo 02/Repetições em Python (for)/desafio049.py
@@ -1,4 +1,4 @@
-numero = int(input("Digite um número: "))
+numero = int(input("Digite um número para ver sua tabuada: "))
 print("=" * 30)
 for c in range(1,11):
     print(f"{numero} x {c} = {numero*c}")
