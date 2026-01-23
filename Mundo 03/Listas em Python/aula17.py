196752f1c50096b1de6d540da55c049fc2340934
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-01-23 17:16:29 -0300

    update

---
+++ Mundo 03/Listas em Python/aula17.py
@@ -0,0 +1,5 @@
+a = [2,3,4,7]
+b = a #fazendo assim não é uma ligação e sim uma copia
+b[2] = 8
+print(a)
+print(b)
---
+++ Mundo 03/Listas em Python/desafio078.py
@@ -0,0 +1,14 @@
+valores = list()
+maior = menor = 0
+for c in range(0, 5):
+    valores.append(int(input(f"Digite o {c + 1} valor: ")))
+    num = valores[c]
+    if c == 0:
+        maior = menor = num
+    if num > maior:
+        maior = num
+    if num < menor:
+        menor = num
+print(f"Voce digitou os valores {valores}")
+print(f"O maior valor digitado foi {maior} na posição {valores.index(maior)+1}...")
+print(f"O menor valor digitado foi {menor} na posição {valores.index(menor)+1}...")
