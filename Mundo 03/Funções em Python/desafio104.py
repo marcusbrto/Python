e9642ba3952128236048ec8fc478c9baba76f7c3
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-02-08 19:27:32 -0300

    Create desafio104.py

---
+++ Mundo 03/Funções em Python/desafio104.py
@@ -0,0 +1,11 @@
+def leiaInt(msg):
+    while True:
+        num = input(msg).strip()
+        if num.isnumeric():
+            return int(num)
+        print("\033[31mERRO! Digite um número inteiro válido.\033[m")
+
+# Programa principal
+n = leiaInt("Digite um número: ")
+print(f"\033[32mVocê acabou de digitar o número {n}\033[m")
+
