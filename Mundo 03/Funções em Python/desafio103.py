6a20d14ae0a2a0140f15612aa2d54d9aee51e7ae
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-02-08 14:49:43 -0300

    Create desafio103.py

---
+++ Mundo 03/Funções em Python/desafio103.py
@@ -0,0 +1,15 @@
+def ficha(jogador, gols):
+    print(f"O jogador {jogador} fez {gols} gols")
+
+nome = input("Digite o nome do jogador: ").strip()
+gols = input("Digite o número de gols: ")
+
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+
+if nome == "":
+    ficha("DESCONHECIDO", gols)
+else:
+    ficha(nome, gols)
