4ccd42df53dd098f2a3d844496093501d7f14d20
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-02-12 13:50:21 -0300

    update

---
+++ Mundo 03/Modularização em Python/desafio107/moeda/__init__.py
@@ -0,0 +1,11 @@
+def aumentar(valor, porcentagem):
+    return valor + (valor * porcentagem/100)
+
+def diminuir(valor, porcentagem):
+    return valor - (valor * porcentagem/100)
+
+def dobro(valor):
+    return valor * 2
+
+def metade(valor):
+    return valor / 2
