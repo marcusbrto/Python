b83928cdae94a591cd84ebdf2a32aecafaa9deef
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-03-18 21:08:13 -0300

    revisao

---
+++ Revisões/saidadedados.py
@@ -0,0 +1,10 @@
+
+# entrada de dados
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+salario = float(input("Digite seu salário: "))
+nacionalidade = str(input("Digite seu nacionalidade: "))
+
+# impressao dos inputs
+
+print(f"Seu nome é {nome}, sua idade é {idade}, você recebe {salario} reais e sua nacionalidade é {nacionalidade}")
