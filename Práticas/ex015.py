c9647933ffbe0cbcd52b3dd61fe86cc0d88ec99f
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-03-20 20:25:36 -0300

    praticando

---
+++ Práticas/ex015.py
@@ -0,0 +1,8 @@
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == "entrar":
+    print("Entrada liberada")
+elif entrada == "sair":
+    print("Saindo..")
+else:
+    print("Entrada invalida")
