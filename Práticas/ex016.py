1a07a39f562a523b6905d9d25f62c19e5babfb30
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-03-20 20:53:42 -0300

    Aplicando gitignore completo

--- Práticas/ex016.py
+++ Práticas/ex016.py
@@ -1,1 +1,17 @@
-"teste"
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('Condicao 1')
+elif condicao2:
+    print('Condicao 2')
+elif condicao3:
+    print('Condicao 3')
+elif condicao4:
+    print('Condicao 4')
+    
+
+print('Fora do IF')
