19383fc2a3f8dc8805d4d4cec800a24471d86684
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-03-22 21:37:21 -0300

    novos exercicios

---
+++ Práticas/ex029.py
@@ -0,0 +1,13 @@
+nome = "Marcus Vinicius"
+
+contador = 0
+novo_nome = ""
+
+while contador < len(nome):
+    letra = nome[contador]
+    novo_nome += f"*{letra}"
+    contador += 1
+    
+print(novo_nome)
+    
+    
