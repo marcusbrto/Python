26e1b4aa41bddc6c9a13280e25ff727a0ee43709
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-04-15 13:18:18 -0300

    aprendizados

--- Práticas/ex043.py
+++ Práticas/ex043.py
@@ -1,4 +1,5 @@
-cpf = "85350005006"
+# cpf = '36440847007'  # Esse CPF gera o primeiro dígito como 10 (0)
+cpf = "74682489070"
 nove_digitos = cpf[:9]
 contador_regressivo_1 = 10
 
@@ -8,17 +9,4 @@ for digito_1 in nove_digitos:
     contador_regressivo_1 -= 1
 digito_1 = (resultado_digito_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
-
-dez_digitos = cpf[:9] + str(digito_1)
-contador_regressivo_2 = 11
-
-resultado_digito_2 = 0
-for digito_2 in nove_digitos:
-    resultado_digito_2 += int(digito_2) * contador_regressivo_2
-    contador_regressivo_2 -= 1
-digito_2 = (resultado_digito_2 * 10) % 11
-digito_2 = digito_2 if digito_2 <= 9 else 0
-
-cpf_completo = cpf[:9] + str(digito_1 + digito_2)
-
-print(cpf_completo)
+print(digito_1)
---
+++ Práticas/ex044.py
@@ -0,0 +1,38 @@
+import re
+import sys
+
+entrada = input("Digite o CPF: ")
+cpf_enviado_cliente = re.sub(r'[^0-9]','',entrada)
+
+entrada_e_sequencial = entrada == entrada[0] * len(entrada)
+
+if entrada_e_sequencial:
+    print("Voce mandou dados sequenciais")
+    sys.exit()
+
+nove_digitos = cpf_enviado_cliente[:9]
+contador_regressivo_1 = 10
+
+resultado_digito_1 = 0
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10 % 11)
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_enviado_cliente == cpf_gerado:
+    print(f"{cpf_enviado_cliente} é válido")
+else:
+    print("CPF inválido")
